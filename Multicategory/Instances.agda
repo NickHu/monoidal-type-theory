bcc6d4509e708bd594085d188876bb7cb8c3cdad
Nick Hu <me@nickhu.co.uk>
2026-07-14 12:03:10 +0900

    flake.nix: use 1lab version of Agda and nixpkgs

---
+++ Multicategory/Instances.agda
@@ -0,0 +1,5 @@
+-- every category is (trivially) a multicategory
+
+open import Multicategory
+
+module Multicategory.Instances where
--- flake.nix
+++ flake.nix
@@ -1,18 +1,51 @@
 {
   description = "Agda with 1lab";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    onelab = {
+      url = "github:the1lab/1lab?rev=e5a99a399a3c58922adef713f38314805810937c";
+      flake = false;
+    };
+  };
 
   outputs =
     {
       self,
-      nixpkgs,
       flake-utils,
+      onelab,
     }:
+    let
+      overlay =
+        final: prev:
+        let
+          Agda = prev.labHaskellPackages.Agda.nodebug.overrideAttrs (previousAttrs: {
+            meta = (previousAttrs.meta or { }) // {
+              mainProgram = "agda";
+            };
+          });
+          agdaPackages = (prev.agdaPackages.override { inherit Agda; }).overrideScope (
+            self: super: {
+              _1lab = super._1lab.overrideAttrs {
+                version = "0-unstable-2026-05-18";
+                src = onelab;
+              };
+            }
+          );
+        in
+        {
+          inherit agdaPackages;
+          agda = agdaPackages.agda;
+        };
+    in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs =
+          (import (onelab + "/support/nix/nixpkgs.nix") {
+            inherit system;
+          }).extend
+            overlay;
       in
       {
         devShells.default = pkgs.mkShell {
